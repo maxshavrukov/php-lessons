da5f1160f1c9ff78d7f58048b4904c1aeff10643
Max Shavrukov <max.shavrukov@gmail.com>
2023-08-22 13:15:04 +0300

    second commit

--- leson11/index.php
+++ leson11/index.php
@@ -4,43 +4,30 @@
 ?>
 <h1>Главная страница</h1>
 <?php
-    $lis = [5, 7, 3, 6, 7, 8];
-    unset($lis[1]);
-    $lis = array_values($lis);
-    rsort($lis);
+//    $file = fopen("text.txt", "a");
 
-    $arr = array_splice($lis, 2, 2);
-    print_r($arr);
-    echo "<br>";
+//    fwrite($file, "\nExample text php\nHello");
 
-    $arr_1 = [5,7];
-    $arr_2 = [6, 8, 9];
-    $arr_3 = array_merge($arr_1, $arr_2);
-    print_r($arr_3);
-    echo "<br>";
+//    fclose($file);
 
+    $filename = "text.txt";
 
-//    if (in_array(3, $lis) == "")
-//        echo "not found";
-//    else
-//        echo "found";
+    $file = fopen($filename, "r");
+    
+    $content = fread($file, filesize($filename));
+    
+    fclose($file);
 
-    print_r($lis);
+    echo "<pre>".$content."</pre><br>";
 
-    echo "<br>";
+    //file_put_contents("a.txt", "Example\nHello");
+    echo file_get_contents("a.txt") . "<br>";
 
-    $x = "10";
-    echo gettype($x). "<br>";
-    echo is_numeric($x). "<br>";
-    echo is_array($x). "<br>";
+    echo file_exists("a.txt") . "<br>";
+    //rename("a.txt", "new_name.txt");
+    unlink("new_name.txt");
 
-    $str = "example";
-    echo strpos($str, "am") . "<br>";
+    echo fileperms(__FILE__);
 
-    $words = "john, bob, alex";
-    $arr_words = explode(",", $words);
-    print_r($arr_words);
-    echo '<br>' .implode(" | ", $arr_words);
-
-    require "blocks/footer.php";
+    require_once "blocks/footer.php";
 ?>
