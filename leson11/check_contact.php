21259d9096b0ea286cbfa4dc693084bc9e893dbb
Max Shavrukov <max.shavrukov@gmail.com>
2023-08-23 10:16:02 +0300

    23.08.2023

--- leson11/check_contact.php
+++ leson11/check_contact.php
@@ -1,28 +1,41 @@
 <?php
     session_start();
 
+    function redirect(){
+        header('Location: contact.php');
+        exit;
+    }
+
     $user_name = htmlspecialchars(trim($_POST['username']));
     $from = htmlspecialchars(trim($_POST['email']));
     $subject = htmlspecialchars(trim($_POST['subject']));
     $message = htmlspecialchars(trim($_POST['message']));
 
-    $_SESSION['username'] = $user_name;
+    $_SESSION['user_name'] = $user_name;
     $_SESSION['email'] = $from;
     $_SESSION['subject'] = $subject;
     $_SESSION['message'] = $message;
 
-    if(strlen($user_name) <= 1)
+    if(strlen($user_name) <= 1) {
         $_SESSION['error_username'] = "Введите корректное имя";
-    else if(strlen($form) < 5 || strpos($form, "@") == false)
+        redirect();
+    }
+    else if(strlen($form) < 5 || strpos($form, "@") == false){
         $_SESSION['error_email'] = "Вы ввели некорректный email";
-    else if (strlen($subject) <= 5)
+        redirect();
+    }
+    else if (strlen($subject) <= 5){
         $_SESSION['error_subject'] = "Тема сообщения не менее 5 символов";
-    else if (strlen($message) <= 15)
+        redirect();
+    }
+    else if (strlen($message) <= 15){
         $_SESSION['error_message'] = "Сообщения не менее 15 символов";
+        redirect();
+    }
     else{
         $_SESSION['error_username'] = "";
         $_SESSION['error_email'] = "";
-        $_SESSION['error_sunject'] = "";
+        $_SESSION['error_subject'] = "";
         $_SESSION['error_message'] = "";
     }
     
--- leson11/contact.php
+++ leson11/contact.php
@@ -6,14 +6,18 @@ require_once "blocks/header.php";
 ?>
 <h1 class="mt-5">Контакты</h1>
 <form action="check_contact.php" method="post">
-    <input type="text" name="username" value="<?= $_SESSION['username'] ?>" placeholder="Введите имя" class="form-control">
+    <input type="text" name="username" value="<?= $_SESSION['user_name'] ?>" placeholder="Введите имя" class="form-control">
     <div class="text-danger"><?= $_SESSION['error_username'] ?></div><br>
+    
     <input type="email" name="email" value="<?= $_SESSION['email'] ?>" placeholder="Введите email" class="form-control">
     <div class="text-danger"><?= $_SESSION['error_email'] ?></div><br>
+    
     <input type="text" name="subject" value="<?= $_SESSION['subject'] ?>" placeholder="Тема сообщения" class="form-control">
     <div class="text-danger"><?= $_SESSION['error_subject'] ?></div><br>
-    <textarea name="massage" placeholder="Ваше сообщение" class="form-control"><?= $_SESSION['message'] ?></textarea><br>
+    
+    <textarea name="message" placeholder="Ваше сообщение" class="form-control"><?= $_SESSION['message'] ?></textarea><br>
     <div class="text-danger"><?= $_SESSION['error_message'] ?></div><br>
+    
     <button type="submit" class="btn btn-success">Отправить</button>
 </form>
 
